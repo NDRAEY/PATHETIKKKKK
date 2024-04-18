b3e44287d28ee5e662b84b173cfc323e3458ae43
VoiEiSeOnSuomiServine <kahdeksantoista@ndraey.net>
2024-04-18 10:29:20 +0900

    Add helpers; Rewrite WindowEmployee

--- View/WindowEmployee.xaml.cs
+++ View/WindowEmployee.xaml.cs
@@ -1,6 +1,9 @@
-﻿using PATHETIKKKKK.ViewModel;
+﻿using PATHETIKKKKK.Helper;
+using PATHETIKKKKK.Model;
+using PATHETIKKKKK.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +28,32 @@ namespace PATHETIKKKKK.View
             InitializeComponent();
 
             PersonViewModel vmPerson = new PersonViewModel();
-            lvEmployee.ItemsSource = vmPerson.ListPerson;
+            RoleViewModel vmRole = new RoleViewModel();
+            List<Role> roles = new List<Role>();
+
+            foreach (Role r in vmRole.ListRole)
+            {
+                roles.Add(r);
+            }
+            
+            ObservableCollection<PersonDPO> persons = new ObservableCollection<PersonDPO>();
+            FindRole finder;
+            
+            foreach (var p in vmPerson.ListPerson)
+            {
+                finder = new FindRole(p.RoleId);
+                Role rol = roles.Find(new Predicate<Role>(finder.RolePredicate));
+                persons.Add(new PersonDPO
+                {
+                    Id = p.Id,
+                    Role = rol.NameRole,
+                    FirstName = p.FirstName,
+                    LastName = p.LastName,
+                    Birthday = p.Birthday
+                });
+            }
+            
+            lvEmployee.ItemsSource = persons;
         }
     }
 }
